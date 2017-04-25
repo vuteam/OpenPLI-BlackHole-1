c5912b6e4708fd1b2c55938376f720bf2c1acc22
RAED <rrrr53@hotmail.com>
2017-04-25 23:57:03 +0300

    Update dvb.cpp

--- lib/dvb/dvb.cpp
+++ lib/dvb/dvb.cpp
@@ -1318,7 +1318,7 @@ int tuner_type_channel_default(ePtr<iDVBChannelList> &channellist, const eDVBCha
 					case iDVBFrontend::feTerrestrial:
 						return 30000;
 					case iDVBFrontend::feATSC:
- 						return 20000;
+						return 20000;
 					default:
 						break;
 				}
