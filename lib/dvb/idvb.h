9c76f35ac77258bdae6d8b5b1fd4407fd8a8bec9
RAED <rrrr53@hotmail.com>
2017-04-29 21:17:34 +0300

    Update idvb.h

--- lib/dvb/idvb.h
+++ lib/dvb/idvb.h
@@ -208,9 +208,9 @@ public:
 
 	eTransportStreamID getParentTransportStreamID() const { return eTransportStreamID(data[6]); }
 	void setParentTransportStreamID( eTransportStreamID tsid ) { data[6]=tsid.get(); }
-	
+
 	int getSourceID() const { return data[7]; }
- 	void setSourceID(int sourceid) { data[7] = sourceid; }
+	void setSourceID(int sourceid) { data[7] = sourceid; }
 
 	eServiceReferenceDVB getParentServiceReference() const
 	{
@@ -279,7 +279,8 @@ public:
 	{
 		cVPID, cMPEGAPID, cTPID, cPCRPID, cAC3PID,
 		cVTYPE, cACHANNEL, cAC3DELAY, cPCMDELAY,
-		cSUBTITLE, cAACHEAPID=12, cDDPPID, cAACAPID, cacheMax
+		cSUBTITLE, cAACHEAPID=12, cDDPPID, cAACAPID,
+		cDATAPID, cPMTPID, cacheMax
 	};
 
 	int getCacheEntry(cacheID);
