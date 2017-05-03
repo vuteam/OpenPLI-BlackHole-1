03bf9bf20d56e1783bc4218f317ea6a2476c8d6d
RAED <rrrr53@hotmail.com>
2017-05-03 16:34:26 +0300

    Update Downloader.py

--- lib/python/Tools/Downloader.py
+++ lib/python/Tools/Downloader.py
@@ -5,7 +5,7 @@ from urlparse import urlparse
 
 class HTTPProgressDownloader(client.HTTPDownloader):
 	def __init__(self, url, outfile, headers=None):
-		client.HTTPDownloader.__init__(self, url, outfile, headers=headers, agent="Enigma2 HbbTV/1.1.1 (+PVR+RTSP+DL;OpenPLi;;;)")
+		client.HTTPDownloader.__init__(self, url, outfile, headers=headers, agent="Enigma2 HbbTV/1.1.1 (+PVR+RTSP+DL;BlackHole;;;)")
 		self.status = None
 		self.progress_callback = None
 		self.deferred = defer.Deferred()
